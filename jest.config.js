35ab3f88d19cf810b050d1f0d5a6638ebc5e979e
SonYoungsung <sysgigigi@gmail.com>
2024-01-05 14:01:52 +0900

    initial set up

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  verbose: true,
+  testEnvironment: 'jsdom',
+  testTimeout: 10000,
+  moduleDirectories: ['node_modules', 'src'],
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.[t|j]sx?$': 'ts-jest',
+    '^.+\\.scss$': 'jest-scss-transform',
+    '^.+\\.svg$': '<rootDir>/config/svgTransform.js'
+  },
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'identity-obj-proxy',
+    uint8arrays: '<rootDir>/node_modules/uint8arrays/cjs/src',
+    multiformats: '<rootDir>/node_modules/multiformats/cjs/src'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js']
+};
